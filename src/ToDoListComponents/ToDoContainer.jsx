2c7d24b543a370b3ece238926755ec5792b014d8
Murphy <109974838+Murphy120814@users.noreply.github.com>
2023-12-09 19:18:51 +0530

    Changes in HTML
    Not Rendered has been removed and change in Title

--- src/ToDoListComponents/ToDoContainer.jsx
+++ src/ToDoListComponents/ToDoContainer.jsx
@@ -9,7 +9,7 @@ function ToDoContainer() {
     <div className="flex flex-col items-center shadow-md p-4">
       <div className="flex justify-between items-center mb-8 w-full p-4">
         <h1 className="text-center font-bold text-3xl "> My To Do List</h1>
-        <button className="outline bg-teal-300">This is button</button>
+        {/* <button className="outline bg-teal-300">This is button</button> */}
       </div>
 
       <ToDoInsert setToDoTaskList={setToDoTaskList} />
